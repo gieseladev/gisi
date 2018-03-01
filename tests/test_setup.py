2256d0a9437b1cc7263e5a6fa91af8c44e9d8b76
Simon Berger <siku2@outlook.de>
2018-03-01 21:17:55 +0100

    maybe now

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,7 +1,28 @@
+import json
+import os
+
 import pytest
+
+
+def pytest_sessionstart(session):
+    from gisi.constants import FileLocations
+    config = {
+        "token": os.environ["DISCORD_TOKEN"],
+        "mongodb_uri": os.environ["MONGODB_URI"],
+        "google_api_key": os.environ["GOOGLE_API_KEY"]
+    }
+    with open(FileLocations.CONFIG, "w+") as f:
+        json.dump(config, f)
 
 
 @pytest.mark.asyncio
 async def test_setup():
     import gisi
     gisi.Gisi()
+
+
+@pytest.mark.asyncio
+async def test_login():
+    import gisi
+    g = gisi.Gisi()
+    await g.run()
