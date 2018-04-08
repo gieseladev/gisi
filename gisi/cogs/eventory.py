e163a64572fe769881898a35a5bcd813aad0f8d7
Simon Berger <siku2@outlook.de>
2018-04-08 21:18:19 +0200

    hinting is important y'all

--- gisi/cogs/eventory.py
+++ gisi/cogs/eventory.py
@@ -1,8 +1,9 @@
 import eventory
+from discord.ext.commands import Bot
 from eventory.ext.discord import EventoryCog
 
 eventory.load_ext("inktory")
 
 
-def setup(bot):
+def setup(bot: Bot):
     bot.add_cog(EventoryCog(bot))
